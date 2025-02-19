d397f5413ccf06714f2f18bd04db32926578293b
ivaansolerr <ivansoler33@gmail.com>
2025-02-19 12:37:42 +0100

    74530257E First things Coordenada

--- hada-p2/Coordenada.cs
+++ hada-p2/Coordenada.cs
@@ -8,5 +8,16 @@ namespace Hada
 {
     internal class Coordenada
     {
+        private int fila;
+        private int columna;
+
+        public int Fila { get; set; }
+        public int Columna { get; set; }
+
+        public Coordenada()
+        {
+            this.fila = 0;
+            this.columna = 0;
+        }
     }
 }
